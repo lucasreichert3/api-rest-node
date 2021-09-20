3e509b7577f221d6cfcf44dfc4859a3d1dde5f00
Lucas Reichert <lucasreichert3@gmail.com>
2021-09-20 20:58:05 -0300

    estoqueModel

---
+++ src/model/EstoqueModel.ts
@@ -0,0 +1,32 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '../database/database.config';
+
+interface Estoque {
+  id: string;
+  nome: string;
+  descricao?: string;
+}
+
+export class EstoqueModel extends Model<Estoque> {}
+
+EstoqueModel.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descricao: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+      sequelize: db,
+      tableName: 'estoque'
+  }
+);
