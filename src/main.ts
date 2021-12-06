8db3bf8282e81086af6e5f8516e6a1630bd8faf7
Lucas Reichert <lucasreichert3@gmail.com>
2021-12-06 20:25:39 -0300

    final

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import db from './database/database.config';
 import { EstoqueProdutoModel } from './model/EstoqueProduto';
 import estoqueRouter from './router/EstoqueRouter';
@@ -6,7 +7,7 @@ import produtoRouter from './router/ProdutoRouter';
 import estoqueProdutoRouter from './router/EstoqueProdutoRouter';
 import autenticacaoRouter from './router/AutenticacaoRouter';
 import usuarioRouter from './router/UsuarioRouter';
-
+import swaggerDocs from './swagger.json';
 
 db.sync().then(async () => {
   console.log('connected to db')
@@ -16,10 +17,12 @@ db.sync().then(async () => {
 const app = express();
 app.use(express.json());
 
+app.use('/api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 const port = 9000;
 
-app.use('/estoque', estoqueRouter)
 app.use('/produto', produtoRouter)
+app.use('/estoque', estoqueRouter)
 app.use('/estoqueProduto', estoqueProdutoRouter)
 app.use('/usuario', usuarioRouter)
 app.use('/', autenticacaoRouter)
